d918261ed7e427ca3654b94344a3a6af10277a79
rater <rater193@gmail.com>
2026-01-14 18:03:31 +0000

    Add EconLinkAPI implementation

---
+++ src/com/rater193/economylink/api/EconLinkAPI.java
@@ -0,0 +1,59 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class EconLinkAPI {
+    private Map<String, Float> economy = new HashMap<String, Float>();
+
+    public EconLinkAPI() { }
+
+    public void AddWallet(String walletName) {
+        if(GetWallet(walletName) == null) {
+            return;
+        }
+
+        economy.put(walletName, 0.0f);
+    }
+
+    public Float GetWallet(String walletName) {
+        return economy.get(walletName);
+    }
+
+    public void RemoveWallet(String walletName) {
+        economy.remove(walletName);
+    }
+
+    public Map<String, Float> ListWallets() {
+        return economy;
+    }
+
+    public void TransferFunds(String fromWallet, String toWallet, double amount) {
+        AddMoney(toWallet, amount);
+        RemoveMoney(fromWallet, amount);
+    }
+
+    public void AddMoney(String walletName, double amount) {
+        if(GetWallet(walletName) == null) {
+            return;
+        }
+
+        Float currentBalance = GetWallet(walletName);
+        economy.put(walletName, currentBalance + (float) amount);
+    }
+
+    public void RemoveMoney(String walletName, double amount) {
+        if(GetWallet(walletName) == null) {
+            return;
+        }
+
+        Float currentBalance = GetWallet(walletName);
+        economy.put(walletName, currentBalance - (float) amount);
+    }
+
+    public void Save(String filepath) {
+        
+    }
+
+    public void Load(String filepath) {
+
+    }
+}
